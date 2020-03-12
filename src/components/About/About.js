454c6e0ff155c7b3dc24e4e7b2e805ebbc3f676e
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-12 14:54:18 -0600

    Setup and import remaining components

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './About.scss';
+
+export const About = () => {
+  return (
+    <div className="About">
+    </div>
+  )
+};
+
+export default About;
