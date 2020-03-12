28bac12b0fd8cee6a4a0cdd593a48e8949c91c26
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-12 14:54:18 -0600

    Setup and import remaining components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import './App.scss';
 
 import { Contact } from '../Contact/Contact';
 import { Gallery } from '../Gallery/Gallery';
+import { Photo } from '../Photo/Photo';
+import { About } from '../About/About';
+import { Splash } from '../Splash/Splash';
 
 class App extends Component {
   constructor() {
---
+++ src/components/Photo/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Photo.scss';
+
+export const Photo = props => {
+  return (
+    <div className="Photo">
+    </div>
+  )
+};
+
+export default Photo;
