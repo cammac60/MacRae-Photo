3dde47f6c412b7c8a3a167de1c882a1a07a1a66c
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-13 00:22:04 -0600

    Add and styles links

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -4,14 +4,45 @@ import { Link } from 'react-router-dom';
 import './Splash.scss';
 
 export const Splash = props => {
+  const { selectedColor, changeColor } = props;
+
   return (
     <div className="Splash">
       <div className="splash-back">
         <div className="blur-wrapper">
           <div className="menu-wrapper">
           <div className="text-wrapper">
-            <h1>MacRae Photo</h1>
+            <h1 style={{color: selectedColor}}>MacRae Photo</h1>
             <div className="options-wrapper-lrg">
+              <div className="link-wrapper">
+                <Link
+                  className="menu-link"
+                  id="photo-link"
+                  to="/gallery"
+                  onMouseEnter={() => changeColor('#FFD700')}
+                  onMouseLeave={() => changeColor('#ffffff')}
+                >
+                  Photos
+                </Link>
+                <Link
+                  className="menu-link"
+                  id="about-link"
+                  to="/about"
+                  onMouseEnter={() => changeColor('#4FD5FF')}
+                  onMouseLeave={() => changeColor('#ffffff')}
+                >
+                  About
+                </Link>
+                <Link
+                  className="menu-link"
+                  id="contact-link"
+                  to="/contact"
+                  onMouseEnter={() => changeColor('#FF7373')}
+                  onMouseLeave={() => changeColor('#ffffff')}
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
           </div>
