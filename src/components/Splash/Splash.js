cb2e4c158a223dd7694016e431a635b8b77ffcd1
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-12 22:23:08 -0600

    Setup background images

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Splash.scss';
+
 export const Splash = props => {
   return (
     <div className="Splash">
-      Splash
+      <div className="splash-back">
+        <div className="blur-wrapper">
+        
+        </div>
+      </div>
     </div>
   )
 };
