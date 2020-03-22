28e6b4803abcd46fa30fb0d8f263ffd6c65bca88
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-21 23:06:25 -0600

    Add happy path tests and post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,16 @@
-export const sendMessage = () => {
-
+export const postMessage = async (email, message) => {
+  const url = 'https://marcaephoto.herokuapp.com/api/v1/messages';
+  const payload = {
+    method: 'POST',
+    body: JSON.stringify({email, message}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const response = await fetch(url, payload);
+  if(!response.ok) {
+    throw new Error('Error: There was a problem sending your message.');
+  }
+  const id = await response.json();
+  return id
 };
