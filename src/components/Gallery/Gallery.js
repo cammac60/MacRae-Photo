f0e73bc0773dc30779a38a6daa111fc8f505e9cd
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-21 23:32:56 -0600

    Add basic gallery styles

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,6 +6,15 @@ import './Gallery.scss';
 export const Gallery = props => {
   return (
     <div className="Gallery">
+      <div className="gallery-back">
+        <div className="blur-wrapper">
+          <header className="gallery-header">
+            <h2 className="gallery-title">Gallery</h2>
+            <div className="gallry-btn-wrapper">
+            </div>
+          </header>
+        </div>
+      </div>
     </div>
   )
 };
