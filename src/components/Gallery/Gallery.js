c4a418e2d5bd68bd96f6974b3215a03953974126
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-21 23:57:08 -0600

    Finish header styles

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -7,10 +7,14 @@ export const Gallery = props => {
   return (
     <div className="Gallery">
       <div className="gallery-back">
-        <div className="blur-wrapper">
+        <div className="blur-wrapper-2">
           <header className="gallery-header">
             <h2 className="gallery-title">Gallery</h2>
-            <div className="gallry-btn-wrapper">
+            <div className="gallery-btn-wrapper">
+              <Link to="/" className="back-btn" id="gallery-back-btn">Back</Link>
+              <a className="ig-link" href="https://www.instagram.com/macrae_5280/?hl=en" target="_blank">
+                <img alt="Instagram Logo" src={require('../../images/icons/instagram.svg')} className="ig-logo-2" fill="#ffffff"/>
+              </a>
             </div>
           </header>
         </div>
