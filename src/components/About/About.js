b982a37bb05c79e503687aab5680e3fa20e7d65d
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-22 23:10:41 -0600

    Fix alt tag for profile photo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ export const About = () => {
         <div className="blur-wrapper">
           <div className="about-wrapper">
             <div className="bio-wrapper">
-              <img alt="Photo of Cameron MacRae" className="profile-img" src={require('../../images/IMG_5947.jpg')} />
+              <img alt="Portrait of Cameron MacRae" className="profile-img" src={require('../../images/IMG_5947.jpg')} />
               <div className="bio-text-wrapper">
                 <p className="bio-text">
                 I am a hobbyist photographer based in Denver, Colorado who specializes in landscape and cityscape. My passion for photography comes from my love of travelling and geography. Being able to document the world around me inspires me to see as much as possible and to step outside of my comfort zone. Some of my bucket list locations to shoot are Japan, San Marino, Morocco, New Zealand, Chile, and Nepal among many others.
