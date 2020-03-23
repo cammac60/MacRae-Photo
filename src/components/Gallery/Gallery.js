620e4008d69fadb6948f576947b8491de6340e94
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-22 23:09:35 -0600

    Add rel tags to ig links

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -24,7 +24,7 @@ export const Gallery = props => {
             <h2 className="gallery-title">Gallery</h2>
             <div className="gallery-btn-wrapper">
               <Link to="/" className="back-btn" id="gallery-back-btn">Back</Link>
-              <a className="ig-link" href="https://www.instagram.com/macrae_5280/?hl=en" target="_blank">
+              <a className="ig-link" href="https://www.instagram.com/macrae_5280/?hl=en" target="_blank" rel="noopener noreferrer">
                 <img alt="Instagram Logo" src={require('../../images/icons/instagram.svg')} className="ig-logo-2" fill="#ffffff"/>
               </a>
             </div>
