14af4d70d7e80b512c6058902b996dc77d11382b
Cameron MacRae <cam.mac.cm@gmail.com>
2020-03-22 22:39:23 -0600

    Finish styles and add to bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ export const About = () => {
               <img alt="Photo of Cameron MacRae" className="profile-img" src={require('../../images/IMG_5947.jpg')} />
               <div className="bio-text-wrapper">
                 <p className="bio-text">
-                I am a hobbyist photographer based in Denver, Colorado who specializes in landscape and cityscape. My passion for photography comes from my love of travelling and geography. Being able to document the world around me inspires me to see as much as possible and to step outside of my comfort zone. Some of my bucket list locations to shoot are Japan, San Marino, Morocco, New Zealand, and Nepal among many others.
+                I am a hobbyist photographer based in Denver, Colorado who specializes in landscape and cityscape. My passion for photography comes from my love of travelling and geography. Being able to document the world around me inspires me to see as much as possible and to step outside of my comfort zone. Some of my bucket list locations to shoot are Japan, San Marino, Morocco, New Zealand, Chile, and Nepal among many others.
                 </p>
                 <br/>
                 <p className="bio-text">
