0798016e7ca39932fb435a65afacfeb7365f6ffc
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-05 15:04:20 -0600

    Add popup text and images

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -8,7 +8,10 @@ export const Popup = () => {
   return (
     <div className="Popup">
       <div className="popup-inner">
-      Text
+        <img src={require('../../images/icons/cancel.svg')} className="exit-btn"/>
+        <h5 className="popup-header">Thank you for your message!</h5>
+        <span className="popup-desc">Please expect a reply to the provided email in 3-5 business days</span>
+        <Link className="popup-back-link">Back to Home</Link>
       </div>
     </div>
   )
