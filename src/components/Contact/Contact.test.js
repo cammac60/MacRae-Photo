ffa237a7f023e1aac09ca6660e092742639baacc
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-06 14:36:11 -0600

    Add handleChange test

--- src/components/Contact/Contact.test.js
+++ src/components/Contact/Contact.test.js
@@ -6,6 +6,13 @@ describe('Contact', () => {
 
   let wrapper, instance;
 
+  const mockEvent = {
+    target: {
+      id: 'email',
+      value: 'test@test.test'
+    }
+  };
+
   beforeEach(() => {
     wrapper = shallow(<Contact />);
     instance = wrapper.instance();
@@ -18,7 +25,23 @@ describe('Contact', () => {
 
   describe('handleChange', () => {
 
-    it('Should change state based on ')
+    it('Should change state based on the id and value of the argument', () => {
+      const mockEventTwo = {
+        target: {
+          id: 'message',
+          value: 'test'
+        }
+      };
+      instance.handleChange(mockEvent);
+      instance.handleChange(mockEventTwo);
+      expect(instance.state).toEqual({
+        email: 'test@test.test',
+        message: 'test',
+        error: '',
+        popUpEnabled: false
+      });
+    });
+
   });
 
 
