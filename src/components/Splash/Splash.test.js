c844b9b8d7a821402ed2d04832a77271758d24f2
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-07 11:32:01 -0600

    Add splash tests

--- src/components/Splash/Splash.test.js
+++ src/components/Splash/Splash.test.js
@@ -5,14 +5,33 @@ import Splash from './Splash';
 describe('Splash', () => {
 
   let wrapper, instance;
+  const changeColor = jest.fn();
+  const mockProps = {
+    changeColor
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<Splash />);
+    wrapper = shallow(<Splash {...mockProps}/>);
     instance = wrapper.instance();
   });
 
   it('Should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should change the color when the cursor enters the photos link', () => {
+    wrapper.find('#photo-link').simulate('mouseEnter');
+    expect(changeColor).toHaveBeenCalledWith('#FFD700');
+  });
+
+  it('Should change the color when the cursor enters the about link', () => {
+    wrapper.find('#about-link').simulate('mouseEnter');
+    expect(changeColor).toHaveBeenCalledWith('#4FD5FF');
+  });
+
+  it('Should change the color when the cursor enters the contact link', () => {
+    wrapper.find('#contact-link').simulate('mouseEnter');
+    expect(changeColor).toHaveBeenCalledWith('#FF7373');
   });
 
 });
