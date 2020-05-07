e765a780bbba7d1d03022e9b77a040893cab0643
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-07 11:15:53 -0600

    Add popup tests

---
+++ src/components/Popup/Popup.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Popup from './Popup';
+
+describe('Popup', () => {
+
+  let wrapper, instance;
+  const closePopup = jest.fn();
+  const mockProps = {
+    closePopup
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<Popup {...mockProps}/>);
+    instance = wrapper.instance();
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should invoke the closePopup method when the close button is clicked', () => {
+    wrapper.find('.exit-btn').simulate('click');
+    expect(closePopup).toHaveBeenCalled();
+  });
+
+});
