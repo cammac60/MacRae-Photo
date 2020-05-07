0f182b4eaf9956a8773f1b7a492c56e3fc60aaeb
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-07 00:37:15 -0600

    Setup photo tests

--- src/components/Photo/Photo.test.js
+++ src/components/Photo/Photo.test.js
@@ -4,15 +4,29 @@ import Photo from './Photo';
 
 describe('Photo', () => {
 
+  const changeImage = jest.fn();
+  const handleArrowClick = jest.fn();
   let wrapper, instance;
+  const mockProps = {
+    changeImage,
+    handleArrowClick,
+    curImgId: 1,
+    image: {
+      title: 'test',
+      location: 'test',
+      link: '25',
+      landscape: false
+    }
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<Photo />);
+    wrapper = shallow(<Photo {...mockProps}/>);
     instance = wrapper.instance();
   });
 
   it('Should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
 
 });
