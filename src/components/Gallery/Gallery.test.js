2e4f4bcd5f9ee36dbe3955c467e3bfa8ca6253fe
Cameron MacRae <cam.mac.cm@gmail.com>
2020-05-07 00:16:15 -0600

    Finish gallery tests

--- src/components/Gallery/Gallery.test.js
+++ src/components/Gallery/Gallery.test.js
@@ -5,14 +5,27 @@ import Gallery from './Gallery';
 describe('Gallery', () => {
 
   let wrapper, instance;
+  const handleImgClick = jest.fn();
+  const mockProps = {
+    handleImgClick,
+    images: [{
+      title: 'test',
+      link: '25'
+    }]
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<Gallery />);
+    wrapper = shallow(<Gallery {...mockProps}/>);
     instance = wrapper.instance();
   });
 
   it('Should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should invoke handleImgClick onClick of the image', () => {
+    wrapper.find('.gallery-tile').simulate('click', {});
+    expect(handleImgClick).toHaveBeenCalled();
   });
 
 });
