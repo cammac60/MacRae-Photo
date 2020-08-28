8fa87161b6f9d57e23e0ea03e51c32f39882e272
Cameron MacRae <cam.mac.cm@gmail.com>
2020-08-27 21:47:24 -0600

    Refactor popup to bem

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -7,12 +7,12 @@ export const Popup = (props) => {
   const { closePopup } = props;
 
   return (
-    <div className="Popup">
-      <div className="popup-inner">
-        <img alt="Exit button" src={require('../../images/icons/cancel.svg')} className="exit-btn" onClick={() => closePopup()}/>
-        <h5 className="popup-header">Thank you for your message!</h5>
-        <span className="popup-desc">Please expect a reply to the provided email in 3-5 business days</span>
-        <Link className="popup-back-link" to="/">Back to Home</Link>
+    <div className="popup">
+      <div className="popup__inner">
+        <img alt="Exit button" src={require('../../images/icons/cancel.svg')} className="popup__button" onClick={() => closePopup()}/>
+        <h5 className="popup__header">Thank you for your message!</h5>
+        <span className="popup__text">Please expect a reply to the provided email in 3-5 business days</span>
+        <Link className="popup__link" to="/">Back to Home</Link>
       </div>
     </div>
   )
